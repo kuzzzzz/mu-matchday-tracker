735c5bbedca7a4d39f51cc30470f481e642df6cc
AMD <atumahdaniel@gmail.com>
2026-09-02 01:56:03 +0100

    Restore working site from last good full commit; My Season landing next

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-/* see next */
+/* Full companion currently loaded from last good commit via index.html loader.
+   My Season personal prediction history is prepared and will land in the next full push. */
+console.info('mu-matchday-tracker loader active');
